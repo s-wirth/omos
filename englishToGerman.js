15f8c8924cc65539740b6e034e988639f5d2d0e8
Sophie Wirth <a.sophiewirth@gmail.com>
2025-07-09 13:14:07 +0200

    format

--- englishToGerman.js
+++ englishToGerman.js
@@ -20,36 +20,39 @@
         oldWords = el.textContent.trim().split(/[^a-z]/gi);
         oldWords.forEach((word) => {
           if (engGerDict[word.toUpperCase()]) {
-            repWord = (/[A-Z]/.test(word[0])) ? engGerDict[word.toUpperCase()].charAt(0).toUpperCase() + engGerDict[word.toUpperCase()].slice(1) : engGerDict[word.toUpperCase()];
+            repWord = /[A-Z]/.test(word[0])
+              ? engGerDict[word.toUpperCase()].charAt(0).toUpperCase() +
+                engGerDict[word.toUpperCase()].slice(1)
+              : engGerDict[word.toUpperCase()];
             el.textContent = el.textContent.replace(word, repWord);
           }
-        })
+        });
       }
     });
   }
   replaceTranslations();
 })();
 
-
 /* Function to test individual bits of code */
 
-(
-  function testStuff(){
-    const elements = document.querySelectorAll("body, body *");
+(function testStuff() {
+  const elements = document.querySelectorAll("body, body *");
 
-    function cLo(){
-      elements.forEach((el) => {
-        if (el.children.length === 0 && el.textContent) {
-          oldWords = el.textContent.trim().split(/[^a-z]/gi);
-          oldWords.forEach((word) => {
-            el.textContent = el.textContent.replace(word, word.toUpperCase());
-          })
-          console.log('el.textContent.trim()', el.textContent.trim());
-          console.log('el.textContent.trim()', el.textContent.trim().split(/[^a-z]/gi));
-        }
-      });
-    }
-
-    cLo();
+  function cLo() {
+    elements.forEach((el) => {
+      if (el.children.length === 0 && el.textContent) {
+        oldWords = el.textContent.trim().split(/[^a-z]/gi);
+        oldWords.forEach((word) => {
+          el.textContent = el.textContent.replace(word, word.toUpperCase());
+        });
+        console.log("el.textContent.trim()", el.textContent.trim());
+        console.log(
+          "el.textContent.trim()",
+          el.textContent.trim().split(/[^a-z]/gi)
+        );
+      }
+    });
   }
-)();
+
+  cLo();
+})();
