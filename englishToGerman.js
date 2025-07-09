1bc73b012116e70fa6353792f9e216c54d691ef0
Sophie Wirth <a.sophiewirth@gmail.com>
2025-07-09 13:13:33 +0200

    rep words considers u&lcase spelling

--- englishToGerman.js
+++ englishToGerman.js
@@ -1,35 +1,55 @@
 (function translateAndLocalize() {
   const engGerDict = {
-    Active: "Aktiv",
-    Subscription: "Abbo",
-    Subscriptions: "Abbos",
+    ACTIVE: "aktiv",
+    PAUSED: "pausiert",
+    NEXT: "nächste",
+    DELIVERY: "Lieferung",
+    SUBSCRIPTION: "Abbo",
+    SUBSCRIPTIONS: "Abbos",
+    SELECTED: "ausgewählte",
+    FLAVOR: "Sorte",
+    FLAVORS: "Sorten",
+    WEEK: "Woche",
+    WEEKS: "Wochen",
+    FOR: "für",
   };
-
-  // 1. Replace English text with German
   function replaceTranslations() {
     const elements = document.querySelectorAll("body, body *");
     elements.forEach((el) => {
       if (el.children.length === 0 && el.textContent) {
-        // console.log('el.textContent.trim()', el.textContent.trim().split(' '));
-        el.textContent = el.textContent
-          .trim()
-          .split(" ")
-          .map((word) => engGerDict[word] || word)
-          .join(" ");
-      }
-      if (el.children.length === 0 && el.textContent.trim() in engGerDict) {
-        console.log('el.textContent', el.textContent);
-        el.textContent = engGerDict[el.textContent.trim()];
-        console.log('el.textContent', el.textContent);
+        oldWords = el.textContent.trim().split(/[^a-z]/gi);
+        oldWords.forEach((word) => {
+          if (engGerDict[word.toUpperCase()]) {
+            repWord = (/[A-Z]/.test(word[0])) ? engGerDict[word.toUpperCase()].charAt(0).toUpperCase() + engGerDict[word.toUpperCase()].slice(1) : engGerDict[word.toUpperCase()];
+            el.textContent = el.textContent.replace(word, repWord);
+          }
+        })
       }
     });
   }
-
-  // 2. Localize Dates and Times
-  // function localizeDates() {
-  // }
-
-  // 3. Run both
   replaceTranslations();
-  // localizeDates();
 })();
+
+
+/* Function to test individual bits of code */
+
+(
+  function testStuff(){
+    const elements = document.querySelectorAll("body, body *");
+
+    function cLo(){
+      elements.forEach((el) => {
+        if (el.children.length === 0 && el.textContent) {
+          oldWords = el.textContent.trim().split(/[^a-z]/gi);
+          oldWords.forEach((word) => {
+            el.textContent = el.textContent.replace(word, word.toUpperCase());
+          })
+          console.log('el.textContent.trim()', el.textContent.trim());
+          console.log('el.textContent.trim()', el.textContent.trim().split(/[^a-z]/gi));
+        }
+      });
+    }
+
+    cLo();
+  }
+)();
