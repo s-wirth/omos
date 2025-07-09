a9891299cc78d027fc1b8a6a160b555394079aa1
Sophie Wirth <a.sophiewirth@gmail.com>
2025-07-09 13:59:16 +0200

    replace textrep function to work with nodes

--- englishToGerman.js
+++ englishToGerman.js
@@ -16,17 +16,30 @@
   function replaceTranslations() {
     const elements = document.querySelectorAll("body, body *");
     elements.forEach((el) => {
-      if (el.children.length === 0 && el.textContent) {
-        oldWords = el.textContent.trim().split(/[^a-z]/gi);
-        oldWords.forEach((word) => {
-          if (engGerDict[word.toUpperCase()]) {
-            repWord = /[A-Z]/.test(word[0])
-              ? engGerDict[word.toUpperCase()].charAt(0).toUpperCase() +
-                engGerDict[word.toUpperCase()].slice(1)
-              : engGerDict[word.toUpperCase()];
-            el.textContent = el.textContent.replace(word, repWord);
+      if (el.textContent) {
+        childTextNodes = [];
+        el.childNodes.forEach((child, index) => {
+          if (child.nodeType === Node.TEXT_NODE) {
+            childText = child.textContent;
+            newText = child.textContent;
+            oldWords = childText.trim().split(/[^a-z]/gi);
+            oldWords.forEach((word) => {
+              if (engGerDict[word.toUpperCase()]) {
+                repWord = /[A-Z]/.test(word[0])
+                  ? engGerDict[word.toUpperCase()].charAt(0).toUpperCase() +
+                    engGerDict[word.toUpperCase()].slice(1)
+                  : engGerDict[word.toUpperCase()];
+                newText = newText.replace(word, repWord);
+              }
+            });
+            childTextNodes.push({ 'ct': childText, 'nt': newText, 'index': index });
+            }
+          if (childTextNodes.length > 0) {
+            childTextNodes.forEach((node) => {
+              el.childNodes[node.index].textContent = node.nt
+            })
           }
-        });
+        })
       }
     });
   }
@@ -54,33 +67,7 @@
 
   function cLo() {
     elements.forEach((el) => {
-      if (el.textContent && el.children.length > 0) {
-        childTextNodes = [];
-        el.childNodes.forEach((child, index) => {
-          if (child.nodeType === Node.TEXT_NODE) {
-            childText = child.textContent;
-            newText = child.textContent;
-            oldWords = childText.trim().split(/[^a-z]/gi);
-            oldWords.forEach((word) => {
-              if (engGerDict[word.toUpperCase()]) {
-                repWord = /[A-Z]/.test(word[0])
-                  ? engGerDict[word.toUpperCase()].charAt(0).toUpperCase() +
-                    engGerDict[word.toUpperCase()].slice(1)
-                  : engGerDict[word.toUpperCase()];
-                newText = newText.replace(word, repWord);
-              }
-            });
-
-            const newNode = document.createTextNode(childText);
-            childTextNodes.push({ 'ct': childText, 'nt': newText, 'index': index });
-          }
-        })
-        if (childTextNodes.length > 0) {
-          const newNode = document.createTextNode("Water");
-          console.log('childTextNodes', childTextNodes)
-        }
-        // console.log('childTextNodes', childTextNodes)
-      }
+      console.log('el', el)
     });
   }
 
